6d35cd926ab72070a3183416b4c8a06fc28e5902
= <=>
2023-10-25 20:09:42 +0500

    project initialized

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$page = 'Textile Recycler, Inc.';
+$pageDesc = '""';
+include 'includes/header.php';
+$page = 'home';
+?>
+<main></main>
+<?php
+include 'includes/footer.php';
+$page = 'home';
+?>
